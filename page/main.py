3b9a86c359ac8eef1f57f593ebcb7d67aa90c197
Nícolas <nicolasauersvalt@gmail.com>
2025-04-15 16:14:40 -0300

    feat: new project

--- page/main.py
+++ page/main.py
@@ -11,21 +11,13 @@ with open(text_path, 'r', encoding='utf-8') as f:
 
     # Função para exibir informações dos pesquisadores
 def exibir_pesquisador(nome, mentor, projetos, linkedin, github):
-    st.write(f"**{nome}**")
-    st.write(f"- **Mentor:** {mentor}")
-    st.write(f"- **Projetos:** {projetos}")
 
     col1, col2 = st.columns(2)
-    
-    with col1:
-        st.link_button("🔗 Linkedin", linkedin)
-
-    with col2:
-        st.link_button("🐙 Portfólio", github)
     
     st.markdown("---")
 
 def inicio():
+    
     st.title("Bem-vindo ao Laboratório Lambda")
 
     # Exibir a imagem principal
@@ -36,35 +28,4 @@ def inicio():
     st.write(dados['mensagem_inicial'])
     st.markdown("---")
 
-    # Pesquisador Principal
-    exibir_pesquisador(
-        dados['pesquisador_principal'],
-        dados['mentor_principal'],
-        dados['projetos_principal'],
-        dados['linkedin_principal'],
-        dados['portfolio_principal']
-    )
-
-    # Pesquisador Associado
-    associado = dados['pesquisador_associado']
-    exibir_pesquisador(
-        associado['nome'],
-        associado['mentor'],
-        associado['projetos'],
-        "https://www.linkedin.com/in/gabriel-trevisani-a811131b5/",
-        "https://github.com/fermathematician"
-    )
-
-    # Pesquisador Assistente
-    assistente = dados['pesquisador_assistente']
-    exibir_pesquisador(
-        assistente['nome'],
-        assistente['mentor'],
-        assistente['projetos'],
-        "https://www.linkedin.com/in/pedro-nascimento-4a35982b0/",
-        "https://github.com/nascpedro"
-    )
-
-    # Exibir o Revisor
-    st.write(f"**Revisor:** {dados['revisor']}")
     st.markdown("---")
